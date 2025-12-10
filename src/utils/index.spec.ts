6cf9db0c0b6b1aa8182c5c571c4b360e23f4ef48
Bartek Strach <iambartekstrach@gmail.com>
2025-12-10 19:06:19 +0100

    chore: remove temp test file

--- src/utils/index.spec.ts
+++
@@ -1,7 +0,0 @@
-import { simpleFuncToConfirmVitestConfig } from '.';
-
-describe('utils', () => {
-    it('returns "true"', () => {
-        expect(simpleFuncToConfirmVitestConfig()).toBe(true);
-    });
-});
