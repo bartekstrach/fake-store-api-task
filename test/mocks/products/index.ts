089d97a9fec237ce799ea36285af14b388cb43b0
Bartek Strach <iambartekstrach@gmail.com>
2025-12-11 18:46:22 +0100

    feat: add product type with mock

--- test/mocks/products/index.ts
+++ test/mocks/products/index.ts
@@ -1,8 +1,20 @@
 import { faker } from '@faker-js/faker';
 
-// TODO: update once types are known
-type Product = { id: string };
+import { Product } from '@/types';
+
 export const mockProduct = (overrides?: Partial<Product>): Product => ({
-    id: faker.string.uuid(),
+    id: faker.number.int({ min: 1, max: 10_000 }),
+    category: faker.word.noun(),
+    description: faker.word.words({ count: { min: 7, max: 10 } }),
+    image: faker.image.url(),
+    price: faker.number.float({ min: 1, max: 10_000, fractionDigits: 2 }),
+    rating: {
+        count: faker.number.int({ min: 1, max: 10_000 }),
+        rate: faker.number.float({ min: 0, max: 5, fractionDigits: 2 }),
+    },
+    title: faker.word.words({ count: { min: 3, max: 5 } }),
     ...overrides,
 });
+
+export const mockProductList = (count: number = 10): Product[] =>
+    Array.from({ length: count }, () => mockProduct());
