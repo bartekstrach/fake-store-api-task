abd054be9403871aa3bcccfb0d94aeb95c015db7
Bartek Strach <iambartekstrach@gmail.com>
2025-12-11 19:49:16 +0100

    feat: add product api type with mock

--- test/mocks/products/index.ts
+++ test/mocks/products/index.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-import { Product } from '@/types';
+import { Product, ProductAPI } from '@/types';
 
 export const mockProduct = (overrides?: Partial<Product>): Product => ({
     id: faker.number.int({ min: 1, max: 10_000 }),
@@ -18,3 +18,20 @@ export const mockProduct = (overrides?: Partial<Product>): Product => ({
 
 export const mockProductList = (count: number = 10): Product[] =>
     Array.from({ length: count }, () => mockProduct());
+
+export const mockProductAPI = (overrides?: Partial<ProductAPI>): ProductAPI => ({
+    id: faker.number.int({ min: 1, max: 10_000 }),
+    category: faker.word.noun(),
+    description: faker.word.words({ count: { min: 7, max: 10 } }),
+    image: faker.image.url(),
+    price: faker.number.float({ min: 1, max: 10_000, fractionDigits: 2 }),
+    rating: {
+        count: faker.number.int({ min: 1, max: 10_000 }),
+        rate: faker.number.float({ min: 0, max: 5, fractionDigits: 2 }),
+    },
+    title: faker.word.words({ count: { min: 3, max: 5 } }),
+    ...overrides,
+});
+
+export const mockProductAPIList = (count: number = 10): ProductAPI[] =>
+    Array.from({ length: count }, () => mockProductAPI());
