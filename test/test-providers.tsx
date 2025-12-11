ba235b51049acc745c36085c5d219a1fcca5e5d2
Bartek Strach <iambartekstrach@gmail.com>
2025-12-11 19:16:28 +0100

    feat: add test wrappers

--- test/test-providers.tsx
+++ test/test-providers.tsx
@@ -1,9 +1,25 @@
 import { ReactNode } from 'react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 interface TestProvidersProps {
     children: ReactNode;
+    queryClient?: QueryClient;
 }
 
-export const TestProviders = ({ children }: TestProvidersProps) => {
-    return <>{children}</>;
+export const TestProviders = ({ children, queryClient }: TestProvidersProps) => {
+    const client =
+        queryClient ??
+        new QueryClient({
+            defaultOptions: {
+                queries: {
+                    retry: false,
+                },
+                mutations: {
+                    retry: false,
+                },
+            },
+        });
+
+    return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
--- test/test-utils.tsx
+++ test/test-utils.tsx
@@ -1,10 +1,44 @@
 import { ReactElement } from 'react';
 
-import { render, RenderOptions } from '@testing-library/react';
+import { QueryClient } from '@tanstack/react-query';
+import {
+    render as rtlRender,
+    RenderOptions,
+    renderHook as rtlRenderHook,
+    RenderHookOptions,
+} from '@testing-library/react';
 
 import { TestProviders } from './test-providers';
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-    render(ui, { wrapper: TestProviders, ...options });
+interface CustomRenderOptions extends Omit<RenderOptions, 'wrapper'> {
+    queryClient?: QueryClient;
+}
 
-export { customRender as render };
+export const render = (ui: ReactElement, options?: CustomRenderOptions) => {
+    const { queryClient, ...renderOptions } = options || {};
+
+    return rtlRender(ui, {
+        wrapper: ({ children }) => (
+            <TestProviders queryClient={queryClient}>{children}</TestProviders>
+        ),
+        ...renderOptions,
+    });
+};
+
+interface CustomRenderHookOptions<Props> extends Omit<RenderHookOptions<Props>, 'wrapper'> {
+    queryClient?: QueryClient;
+}
+
+export const renderHook = <Result, Props>(
+    hook: (props: Props) => Result,
+    options?: CustomRenderHookOptions<Props>
+) => {
+    const { queryClient, ...renderOptions } = options || {};
+
+    return rtlRenderHook(hook, {
+        wrapper: ({ children }) => (
+            <TestProviders queryClient={queryClient}>{children}</TestProviders>
+        ),
+        ...renderOptions,
+    });
+};
