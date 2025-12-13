4ecc30ea13359a86e3916181df36c378651af146
Bartek Strach <iambartekstrach@gmail.com>
2025-12-13 17:21:44 +0100

    feat: add cart summary component

---
+++ src/components/cart-summary/index.tsx
@@ -0,0 +1,41 @@
+import { Button } from '@/components';
+import { CURRENCY } from '@/constants';
+import { formatPrice } from '@/utils';
+
+interface CartSummaryProps {
+    itemCount: number;
+    totalPrice: number;
+}
+
+export const CartSummary = ({ itemCount, totalPrice }: CartSummaryProps) => (
+    <>
+        <div className="card sticky space-y-4">
+            <div className="space-y-2">
+                <div className="text-md flex justify-between">
+                    <span className="text-gray-700">
+                        {itemCount === 1 ? 'Item (1)' : `Items (${itemCount})`}
+                    </span>
+                    <span className="font-medium">{formatPrice(totalPrice, CURRENCY)}</span>
+                </div>
+                <div className="text-md flex justify-between">
+                    <span className="text-gray-700">Shipping</span>
+                    <span className="font-medium text-green-700">Free</span>
+                </div>
+            </div>
+
+            <div className="flex justify-between text-lg font-bold">
+                <span>Total</span>
+                <span>{formatPrice(totalPrice, CURRENCY)}</span>
+            </div>
+
+            <Button
+                className="w-full"
+                disabled={itemCount === 0}
+                onClick={() => alert('Proceeding to Checkout...')}
+                variant="primary"
+            >
+                Proceed to Checkout
+            </Button>
+        </div>
+    </>
+);
