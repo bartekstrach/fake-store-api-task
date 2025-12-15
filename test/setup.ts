a4bb802fb4031170d78a63a4ef8f9bef75f08b25
Bartek Strach <iambartekstrach@gmail.com>
2025-12-15 14:40:40 +0100

    feat: add vitest-canvas-mock, useful for a11y tests

--- test/setup.ts
+++ test/setup.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect } from 'vitest';
 import * as matchers from 'vitest-axe/matchers';
+import { setupVitestCanvasMock } from 'vitest-canvas-mock';
 
 import { server } from '@test/utils';
 
@@ -9,6 +10,10 @@ expect.extend(matchers);
 
 beforeAll(() => {
     server.listen({ onUnhandledRequest: 'error' });
+});
+
+beforeEach(() => {
+    setupVitestCanvasMock();
 });
 
 afterEach(() => {
