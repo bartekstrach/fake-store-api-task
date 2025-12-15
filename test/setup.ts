7791a6c49903ba6ed682e3bc980447d690015e7a
Bartek Strach <iambartekstrach@gmail.com>
2025-12-15 10:47:11 +0100

    feat: add vitest-axe lib

--- test/setup.ts
+++ test/setup.ts
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, expect } from 'vitest';
+import * as matchers from 'vitest-axe/matchers';
 
 import { server } from '@test/utils';
+
+expect.extend(matchers);
 
 beforeAll(() => {
     server.listen({ onUnhandledRequest: 'error' });
